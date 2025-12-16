c679e07affcad0da160254879a680aae714d5987
Naveen <navinmachari77@gamil.com>
2025-12-16 20:48:56 +0530

    initial commit

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,14 +1,37 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
-
+import {toast} from 'react-toastify'
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
 
     const currency = '₹';
     const delivery_fee = 100;
-    const [search,setSearch] = useState('');
-    const [showSearch,setShowSearch] = useState(false);
+    const [search, setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => {
+        if(!size){
+            toast.error('Select Product Size');
+            return;
+        }
+        let cartData = structuredClone(cartItems);
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            }
+            else {
+                cartData[itemId][size] = 1;
+            }
+        }
+        else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
+
     const value = {
         products,
         currency,
@@ -16,8 +39,13 @@ const ShopContextProvider = (props) => {
         search,
         setSearch,
         showSearch,
-        setShowSearch
+        setShowSearch,
+        cartItems,
+        addToCart
     }
+    useEffect(()=>{
+        console.log(cartItems);
+    },[cartItems])
     return (
         <ShopContext.Provider value={value}>
             {props.children}
