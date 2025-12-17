a56362f2286155af9d4dd7de6be8df7da4b7d3d8
Naveen <navinmachari77@gamil.com>
2025-12-17 17:12:31 +0530

    initial commit

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,18 +1,20 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
 
     const currency = '₹';
-    const delivery_fee = 100;
+    const delivery_fee = 20;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const navigate = useNavigate()
 
     const addToCart = async (itemId, size) => {
-        if(!size){
+        if (!size) {
             toast.error('Select Product Size');
             return;
         }
@@ -32,6 +34,45 @@ const ShopContextProvider = (props) => {
         setCartItems(cartData);
     }
 
+    const getCartCount = () => {
+        let totalCount = 0;
+        for (const items in cartItems) {
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalCount += cartItems[items][item];
+                    }
+                } catch (error) {
+
+                }
+            }
+        }
+        return totalCount;
+    }
+
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId][size] = quantity;
+        setCartItems(cartData);
+    }
+
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items);
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalAmount += itemInfo.price * cartItems[items][item];
+                    }
+                } catch (error) {
+
+                }
+            }
+        }
+        return totalAmount;
+    }
+
     const value = {
         products,
         currency,
@@ -41,11 +82,13 @@ const ShopContextProvider = (props) => {
         showSearch,
         setShowSearch,
         cartItems,
-        addToCart
+        addToCart,
+        getCartCount,
+        updateQuantity,
+        getCartAmount,
+        navigate
     }
-    useEffect(()=>{
-        console.log(cartItems);
-    },[cartItems])
+
     return (
         <ShopContext.Provider value={value}>
             {props.children}
